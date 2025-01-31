3224ae908ae759eaa4559e1000e151afccd16f86
jdmartens <jeremydmartens@gmail.com>
2025-01-30 22:19:41 -0600

    update home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import MagicEightBall from '@/components/MagicEightBall.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <MagicEightBall />
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
